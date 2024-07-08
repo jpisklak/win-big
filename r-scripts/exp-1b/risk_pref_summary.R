6eb18c2b548d27b11200079592dd95c2a17a43ac
Jeffrey Pisklak <pisklak@ualberta.ca>
2024-07-08 17:03:02 -0600

    Updated exp-1b analysis and summary files.

--- r-scripts/exp-1b/risk_pref_summary.R
+++ r-scripts/exp-1b/risk_pref_summary.R
@@ -6,22 +6,9 @@
 # source("r-scripts/exp-1b/risk_pref_analysis.R")
 #-------------------------------------------------------------------------------
 
-
 # Main effects
 # -----------------------------------------------------------------------------
 risky_main <- data.frame(risky_main)[, 2:ncol(risky_main)]
-
-# #Nagelkerke (Cragg and Uhler) Pseudo R-squared
-# risky_main$R2 <- c(
-#   NA,
-#   nagelkerke(grp_mod, null = base_mod)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(value_mod, null = grp_mod)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(blk_mod, null = grp_mod)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(grp_val, null = value_mod)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(grp_blk, null = grp_val)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(val_blk, null = grp_blk)$Pseudo.R.squared.for.model.vs.null[3],
-#   nagelkerke(grp_val_blk, null = val_blk)$Pseudo.R.squared.for.model.vs.null[3]
-# )
 
 # Inverse Bayes Factor
 delta_BIC <- risky_main$BIC[2:nrow(risky_main)] - 
@@ -46,22 +33,7 @@ p_vals <- ifelse(risky_main$p.value < .001, '< .001',
 # Inverse Bayes Factor
 bfs <-
   ifelse(risky_main$BF_10 > 150, '> 150',
-    ifelse(risky_main$BF_10 < .01, '< .01',
-      round(risky_main$BF_10, 3)
-    )
+         ifelse(risky_main$BF_10 < .01, '< .01',
+                round(risky_main$BF_10, 3)
+         )
   )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
